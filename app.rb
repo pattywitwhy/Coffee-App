0fa164b9a0071a404a89fdaacfadfa997c789324
pattywitwhy <pattyxhwang@gmail.com>
2019-04-26 14:02:31 -0500

    added to index.erb file

--- app.rb
+++ app.rb
@@ -1,6 +1,36 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+get '/' do
+  erb :index
+end
+
+get "/espresso" do
+  erb:espresso
+end
+
+get "/cappucino" do
+  erb:cappucino
+end
+
+get "/americano" do
+  erb:americano
+end
+
+get "/latte" do
+  erb:latte
+end
+
+get "/mocha" do
+  erb:mocha
+end
+
+get "/macchiato" do
+  erb:macchiato
+end
+
+
+
 get "/espresso" do
   "<h1>Espresso:</h1>
   <p>Just espresso</p>"
