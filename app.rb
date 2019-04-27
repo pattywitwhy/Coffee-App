7236243174529661483c4556ce88749d59babb68
pattywitwhy <pattyxhwang@gmail.com>
2019-04-27 15:55:01 -0500

    api routes added

--- app.rb
+++ app.rb
@@ -1,16 +1,15 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
+require 'sinatra/namespace'
 
 current_dir = Dir.pwd
 Dir["#{current_dir}/models/*.rb"].each { |file| require file }
-
 
 get '/' do
   @drinks = Drink.all
   erb :index
 end
-
 
 get '/drinks/new' do
   erb :new
@@ -28,4 +27,18 @@ end
 get "/drinks/:id" do
   @drink = Drink.find(params[:id])
   erb :show
+end
+
+namespace "/api" do
+  get "/drinks" do
+    content_type 'application/json'
+    @drinks = Drink.all
+    @drinks.to_json
+  end
+
+  get "/drinks/:id" do
+    content_type 'application/json'
+    @drink = Drink.find(params[:id])
+    @drink.to_json
+  end
 end
