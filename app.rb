fc332935990a90b212cf32cf225bbbd49966fc9e
pattywitwhy <pattyxhwang@gmail.com>
2019-04-26 19:21:02 -0500

    added the files to view folders

--- app.rb
+++ app.rb
@@ -28,35 +28,3 @@ end
 get "/macchiato" do
   erb:macchiato
 end
-
-
-
-get "/espresso" do
-  "<h1>Espresso:</h1>
-  <p>Just espresso</p>"
-end
-
-get "/cappucino" do
-  "<h1>Cappucino:</h1>
-  <p>espresso, steamed milk & foam</p>"
-end
-
-get "/americano" do
-  "<h1>Americano:</h1>
-  <p>espresso & hot water</p>"
-end
-
-get "/latte" do
-  "<h1>Latte:</h1>
-  <p>espresso & steamed milk</p>"
-end
-
-get "/mocha" do
-  "<h1>Mocha:</h1>
-  <p>espresso & chocolate</p>"
-end
-
-get "/macchiato" do
-  "<h1>Macciato:</h1>
-  <p>espresso & foam</p>"
-end
